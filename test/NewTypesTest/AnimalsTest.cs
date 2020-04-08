f00dda3c8634a86c5713c09d491b5a24f8baf6c8
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-08 12:19:46 +0300

    Add Tests

--- test/NewTypesTest/AnimalsTest.cs
+++ test/NewTypesTest/AnimalsTest.cs
@@ -1,18 +1,48 @@
+using System;
 using NUnit.Framework;
+using Animals;
 
-namespace NewTypesTest
+public class PetTests
 {
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+  [Test]
+  public void DogTalkToOwnerReturnsWoof()
+  {
+    string expected = "Woof!";
+    string actual = new Dog().TalkToOwner();
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+    Assert.AreEqual(expected, actual, "Dog should say Woof!");
+  }
+
+  [Test]
+  public void RatTalkToOwnerReturnsSqueek()
+  {
+    string expected = "Squeek!";
+    string actual = new Rat().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Rat should say Squeek!");
+  }
+  [Test]
+  public void CatTalkToOwnerReturnsMeow()
+  {
+    string expected = "Meow!";
+    string actual = new Cat().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Cat should say Meow!");
+  }
+  [Test]
+  public void BirdTalkToOwnerReturnsTweet()
+  {
+    string expected = "Tweet!";
+    string actual = new Bird().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Bird should say Tweet!");
+  }
+  [Test]
+  public void PigTalkToOwnerReturnsOink()
+  {
+    string expected = "Oink!";
+    string actual = new Pig().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Pig should say Oink!");
+  }
 }
