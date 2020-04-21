9206e00fab817a4ea3fd03248848f3bc19c79ef5
Hmnp <hasan.mainali@centria.fi>
2020-04-21 12:53:56 +0300

    Bird edited

--- src/Fauna/Animals/Tame/Bird.cs
+++ src/Fauna/Animals/Tame/Bird.cs
@@ -1,10 +1,21 @@
 namespace Animals
 {
-  public class Bird
+  public class Bird : ITame
   {
     public string TalkToOwner()
     {
       return "Tweet!";
     } 
+    
+    public string MakeSound()
+  {
+     return "Tweet!";
   }
+  public string ComeHere()
+  {
+      return "Bird came to the owner";
+  }
+
+  }
+
 }
