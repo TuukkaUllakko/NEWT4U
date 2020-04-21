b8bb3c40bc7b0b9110248a348c43c04bcfc8f643
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-21 15:42:55 +0300

    Pig restored

--- src/Fauna/Animals/Tame/Pig.cs
+++ src/Fauna/Animals/Tame/Pig.cs
@@ -2,24 +2,11 @@ using System;
 
 namespace Animals
 {
-    public class Pig : Animal, ITame
+    public class Pig
     {
-        public string Name { get; set; }
-        public Pig(string Name)
-        {
-            this.Name = Name;
-        }
         public string TalkToOwner()
         {
             return "Oink!";
-        }
-        public override string MakeSound()
-        {
-            return "Oink!";
-        }
-        public string ComeHere()
-        {
-            return this.Name + " came to the owner.";
         }
     }
 }
